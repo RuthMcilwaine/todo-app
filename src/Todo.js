c3e6bdd24239cd9c0ffb652d8f8704ea26df051e
Ruth.Mcilwaine <Ruth.Mcilwaine@myob.com>
2025-02-25 12:35:56 +1100

    updates

--- src/Todo.js
+++ src/Todo.js
@@ -1,41 +1,41 @@
 import React, { useState } from 'react';
 
 const Todo = ({ id, task, removeTodo, editTodo }) => {
-    const [isEditing, setIsEditing] = useState(false);
-    const [newTask, setNewTask] = useState(task);
+  const [isEditing, setIsEditing] = useState(false);
+  const [newTask, setNewTask] = useState(task);
 
   const handleRemove = () => {
     removeTodo(id);
   };
 
-    const handleEdit = () => {
-        setIsEditing(true);
-    };
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
 
-    const handleSave = () => {
-        editTodo(id, newTask);
-        setIsEditing(false);
-    };
+  const handleSave = () => {
+    editTodo(id, newTask);
+    setIsEditing(false);
+  };
 
-    const handleChange = (e) => {
-        setNewTask(e.target.value);
-    };
+  const handleChange = (e) => {
+    setNewTask(e.target.value);
+  };
 
   return (
     <div>
-        {isEditing ? (
-            <div>
-                <input type="text" value={newTask} onChange={handleChange} />
-                <button onClick={handleSave}>Save</button>
-            </div>
-        ) : (
-            <div>
-                <li>{task}</li>
-                <button onClick={handleEdit}>Edit</button>
-      <button onClick={handleRemove}>X</button>
-    </div>
-  )}
+      {isEditing ? (
+        <div>
+          <input type="text" value={newTask} onChange={handleChange} />
+          <button onClick={handleSave}>Save</button>
         </div>
+      ) : (
+        <div>
+          <li>{task}</li>
+          <button onClick={handleEdit}>Edit</button>
+          <button onClick={handleRemove}>X</button>
+        </div>
+      )}
+    </div>
   );
 };
 
